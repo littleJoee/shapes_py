fb1dac0c52bd412e5661914470b885f43fcee471
Joseph Owolabi <josephowolabi428@gmail.com>
2025-11-09 15:35:54 +0100

    remade the game over screen in the main game loop and made the timer start when you made your first input.
    play again dosent work (not sure how to do that yet)

--- gems.py
+++ gems.py
@@ -12,10 +12,14 @@ class GemHub:
                             'triangle': [False, False, True, False], 
                             'diamond': [True, False, False, False]}
         
+        self.current_state = 'start'
         self.current_gem_type = self.gem_types[1]
         self.gem_map = []
 
-    def update(self, direction):
+    def update(self, direction, state):
+        if state != self.current_state:
+            self.current_state = state
+
         if direction[0]: # left
             self.correct = False
             pos = (random.randint(0, 120), random.randint(150, 300))
@@ -31,23 +35,23 @@ class GemHub:
     
     def append_gem(self, pos):
         '''adds gem to a dictionary to keep track for drawing'''
-        self.gem_map.append({'type': self.current_gem_type, 'pos': pos, 's': [3, 3]})
+        self.gem_map.append({'type': self.current_gem_type, 'pos': [pos[0], pos[1]], 's': [3, 3]})
 
     def change_gem(self):
         '''change to a random gem'''
         self.current_gem_type = self.gem_types[random.randint(0, len(self.gem_types) - 1)]
     
     def funmode(self):
-        '''dvd like bouncing'''
+        '''dvd like bouncing on gems'''
         # bouncing off side logic
         for gem in self.gem_map:
-            if gem['pos'][0] <= 0 or gem['pos'][0] + 50 <= self.game.screen_width:
-                gem['s'[0]] -= gem['s'[0]]
-            elif gem['pos'][1] <= 0 or gem['pos'][1] + 50 <= self.game.screen_width:
-                gem['s'[1]] -= gem['s'[1]]
+            if gem['pos'][0] <= 0 or gem['pos'][0] + 50 >= self.game.screen_width:
+                gem['s'][0] = -gem['s'][0]
+            elif gem['pos'][1] <= 0 or gem['pos'][1] + 50 >= self.game.screen_width:
+                gem['s'][1] = -gem['s'][1]
 
-            gem['pos'] += gem['s'[0]]
-            gem['pos'] += gem['s'[1]]
+            gem['pos'][0] += gem['s'][0]
+            gem['pos'][1] += gem['s'][1]
         
     def is_correct(self, direction):
         if direction == self.correct_dir[self.current_gem_type]:
@@ -58,7 +62,7 @@ class GemHub:
 
 
     # displays gem in te middle and other gems(appended to a dictionary and then drawn like tilemaps)
-    def render(self, surf): 
+    def render(self, surf):
         for gem in self.gem_map:
             surf.blit(self.assets[gem['type']], gem['pos'])
         surf.blit(self.assets[self.current_gem_type], self.pos)
